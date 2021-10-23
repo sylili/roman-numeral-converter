22014bd5206936e1bf094647c01c3afa5619d3d4
Lilla Illesy <lilla.illesy@bbraun.com>
2021-10-23 08:52:51 +0200

    Clean-up of initial default ReactApp

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('title is presented', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleEl = screen.getByText(/Arabic to roman numeral converter/i);
+  expect(titleEl).toBeInTheDocument();
 });
