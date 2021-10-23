d7408a0f89fd2198cd1dca555c2e862e262423e1
Lilla Illesy <lilla.illesy@bbraun.com>
2021-10-23 11:00:37 +0200

    Input field added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+
+  const handleChange = (event) => {
+    setInputText(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">        
-        <p>
-          Arabic to roman numeral converter.
-        </p>
+      <header className="App-header">
+        <p>Arabic to roman numeral converter.</p>
+        <input value={inputText} onChange={handleChange} />
       </header>
     </div>
   );
